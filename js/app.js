f4656b23a2b5e8187eaa5661ccd1598e8da634ff
cgoodwill <christy.goodwill@gmail.com>
2015-12-09 21:19:56 -0700

    add function to fizzbuzz

--- js/app.js
+++ js/app.js
@@ -1,25 +1,29 @@
+function fizzOrBuzz(topNumber) {
+
+    for (i = 1; i <= topNumber; i++) {
+
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log("fizzbuzz");
+            $('.list').append('<li>fizzbuzz</li>');
+        } else if (i % 3 == 0) {
+            console.log("fizz");
+            $('.list').append('<li>fizz</li>');
+        } else if (i % 5 == 0) {
+            console.log("buzz");
+            $('.list').append('<li>buzz</li>');
+        } else {
+            console.log(i);
+            $('.list').append('<li>' + i + '</li>');
+        }
+    }
+}
+
 $(document).ready(function() {
-	// alert('test');
-	console.log("start");
 
-	// user number's pick
-	var userVariable = prompt("Pick a number between 1 - 100?");
+   console.log("start");
 
-	for (i = 1; i <= userVariable; i++) {
-	  // console.log(i);
-	  if (i % 3 == 0 && i % 5 == 0) {
-	    console.log("fizzbuzz");
-	    $('.list').append('<li>fizzbuzz</li>');
-	  } else if (i % 3 == 0) {
-	    console.log("fizz");
-	    $('.list').append('<li>fizz</li>');
-	  } else if (i % 5 == 0) {
-	    console.log("buzz");
-	    $('.list').append('<li>buzz</li>');
-	  } else {
-	    console.log(i);
-	    $('.list').append('<li>' + i + '</li>');
-	  }
-	}
+   // user number's pick
+   var userVariable = prompt("Pick a number between 1 - 100?");
 
+   fizzOrBuzz(userVariable)
 });
